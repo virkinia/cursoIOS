0f2f2e68e86ab21c5aaf198e8e74719961fa085d
Test <test@test.com>
2019-04-08 17:02:09 +0200

    Initial Commit

---
+++ SegundaMano/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegundaMano
+//
+//  Created by Dev2 on 08/04/2019.
+//  Copyright © 2019 CFTIC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
